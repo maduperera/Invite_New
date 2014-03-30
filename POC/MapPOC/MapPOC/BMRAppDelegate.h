00fc1ea7b41950345dd57289ecf4b6ac8d000280
madusha perera <madupiz@gmail.com>
2014-03-31 01:03:07 +0530

    poc on map : successful with drag able annotations

---
+++ POC/MapPOC/MapPOC/BMRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BMRAppDelegate.h
+//  MapPOC
+//
+//  Created by Madusha Perera on 3/30/14.
+//  Copyright (c) 2014 Madusha Perera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
