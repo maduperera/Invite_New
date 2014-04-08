d7a14ea9535fbfdbc20fd609f042054f00098862
dfernando123 <dhammini.dev@gmail.com>
2014-04-08 18:09:45 +0530

    POC

---
+++ POC/HangIn/HangIn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HangIn
+//
+//  Created by Dhammini Fernando on 4/8/14.
+//  Copyright (c) 2014 Dhammini Fernando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
